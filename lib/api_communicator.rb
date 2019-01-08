5d67b6ceae5aa8f4c037f2d76f42cbe2a50f6fa6
John Oncher <johnoncher@gmail.com>
2019-01-08 16:09:50 -0500

    api and iteration

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,33 +1,45 @@
-require 'rest-client'
-require 'json'
-require 'pry'
+require "rest-client"
+require "json"
+require "pry"
+
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+ response_string = RestClient.get("http://www.swapi.co/api/people/")
+ response_hash = JSON.parse(response_string)
+ response_hash['results'].each do |x,y|
+   x.each do |key, val|
+     array = []
+     if key == 'films'
+        val.each do |film|
+          hash = {}
+          response_string2 = RestClient.get(film)
+          response_hash2 = JSON.parse(response_string2)
+          array.push(response_hash2)
+        end
+        return array
+      end
+    end
+ end
 end
+
+
+
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+
+ films.each do |movie|
+   movie.each do |info, data|
+     if info == 'title'
+       puts data
+     end
+   end
+  end
 end
+
+
 
 def show_character_movies(character)
-  films = get_character_movies_from_api(character)
-  print_movies(films)
+ films = get_character_movies_from_api(character)
+ print_movies(films)
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+
 def welcome
-  # puts out a welcome message here!
+ puts "Welcome stranger from a far far away galaxy"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+ puts 'please enter a character name'
+ character_name = gets.chomp
+ return character_name.downcase
+ # use gets to capture the user’s input. This method should return that input, downcased.
 end
